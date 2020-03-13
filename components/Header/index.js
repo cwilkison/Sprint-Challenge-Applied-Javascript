fd70d400829c6a32b11a58f93915452d59fb1913
Cole Wilkison <wilkison.cole@gmail.com>
2020-03-13 11:40:11 -0400

    header done

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const headerMain = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+
+    headerMain.appendChild(headerDate);
+    headerMain.appendChild(headerTitle);
+    headerMain.appendChild(headerTemp);
+
+    headerMain.classList.add('header');
+    headerDate.classList.add('date');
+    headerTemp.classList.add('temp')
+
+    headerDate.textContent = `SMARCH 28, 2019`;
+    headerTitle.textContent = `Lambda Times`;
+    headerTemp.textContent = `98*`;
+
+
+
+        return headerMain
+      
+      };
+
+
+      const newHeader = document.querySelector('.header-container');
+        newHeader.appendChild(Header());
