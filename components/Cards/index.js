2b46aca4bc9fcedd4749986e3479cb0ccd7b430d
Cole Wilkison <wilkison.cole@gmail.com>
2020-03-13 14:06:41 -0400

    working on cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,60 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles');
+
+function createCard(info){
+    const cardMain = document.createElement('div');
+    const cardHeadline = document.createElement('div');
+    const cardAuthor = document.createElement('div');
+    const cardContainer = document.createElement('div');
+    const cardImage = document.createElement('img');
+    const cardSpan = document.createElement('span');
+
+    cardMain.appendChild(cardHeadline);
+    cardMain.appendChild(cardAuthor);
+    cardAuthor.appendChild(cardContainer);
+    cardContainer.appendChild(cardImage);
+    cardContainer.appendChild(cardSpan);
+
+    cardMain.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthor.classList.add('author');
+    cardContainer.classList.add('img-container');
+
+    cardHeadline.textContent =`${info.headline}`;
+    cardImage.src = `${info.authorPhoto}`;
+    cardSpan.textContent = `By ${info.authorName}`;
+
+    return cardMain
+
+}
+
+
+const cards = document.querySelector('cards-container');
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+response.data.articles.forEach(javascript => {
+    javascript.forEach(items => {
+        cards.append(createCard(items));
+    })
+    })
+  })
+    .catch(error => {
+      console.log("error", error);
+    });
+
+/*
+const newTab = document.querySelector('.topics');
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response => {
+    response.data.topics.forEach(item => {
+        newTab.appendChild(Tab(item));
+});
+})
+    .catch(error => {
+        console.log("error", error)
+    })
+
+    */
